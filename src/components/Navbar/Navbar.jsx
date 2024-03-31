b335cb693dc78fe1bbf3366a83dba231dfe98b06
Pietro Costanzi Fantini <pietrocostanzi39@gmail.com>
2024-04-01 01:12:57 +0200

    fixato gli elementi della navbar. IMPORTANTE: dobbiamo aggiungere la componente delle regole al piu' presto visto che lo scroll necessita di una componente con id GameRules

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,23 @@
 import './Navbar.scss'
+import { useEffect, useRef } from 'react'
 import { Link } from 'react-scroll'
-import { animateScroll } from 'react-scroll';
+import { Link as LinkRouter, useLocation, useNavigate } from 'react-router-dom'
 import logo from '../../assets/logo_circle.png'
 
 
 export default function Navbar(){
 
+    const location = useLocation();
+
+    const handleRulesClick = (event) => {
+        setTimeout(() => {
+            event.preventDefault();
+            const gameRulesElement = document.getElementById('GameRules');
+            gameRulesElement?.scrollIntoView({ behavior: 'smooth' });
+        },300);
+    }
+
+    const isRoot = location.pathname === "/";
     return(
         <section id='Navbar'>
             <nav className="navbar fixed-top navbar-expand-lg navbar-dark p-3" id="headerNav">
@@ -20,10 +32,18 @@ export default function Navbar(){
                     <div className=" collapse navbar-collapse" id="navbarNavDropdown">
                     <ul className="navbar-nav mx-auto align-items-center">
                         <li className=" nav-item">
-                            <Link activeClass="navLinkActive" className="navElem nav-link mx-2" to="Carousel" spy={true} smooth={true} duration={800}>Home</Link>
+                            {isRoot ? (
+                                <Link activeClass="navLinkActive" className="navElem nav-link mx-2" to="Leaderboard" spy={true} smooth={true} duration={800}>Home</Link>
+                            ) : (
+                                <LinkRouter className="navElem nav-link mx-2" to="/">Home</LinkRouter>
+                            )}
                         </li>
                         <li className="long navElem nav-item">
-                            <Link activeClass="navLinkActive" className="nav-link mx-2" to="Products" spy={true} smooth={true} duration={800}>Products</Link>
+                            {isRoot ? (
+                                <Link activeClass="navLinkActive" className="nav-link mx-2" to="GameRules" spy={true} smooth={true} duration={800}>Rules</Link>
+                            ) : (
+                                <LinkRouter className="nav-link mx-2" onClick={handleRulesClick} to="/">Rules</LinkRouter>
+                            )}
                         </li>
                         <li className="nav-item d-none d-lg-block">
                             <Link id='logoNav' className="nav-link mx-2" to='Carousel'>
@@ -31,10 +51,10 @@ export default function Navbar(){
                             </Link>
                         </li>
                         <li className="navElem nav-item">
-                            <Link className="nav-link mx-2" aria-selected='false' aria-controls='tab-pricing' to="Pricing">Pricing</Link>
+                            <LinkRouter className="nav-link mx-2" aria-selected='false' aria-controls='tab-pricing' to="/Tech">Tech</LinkRouter>
                         </li>
                         <li className="long navElem nav-item">
-                            <Link className="nav-link mx-2" aria-selected='false' aria-controls='tab-aboutus' to="AboutUs">About us</Link>
+                            <LinkRouter className="nav-link mx-2" aria-selected='false' aria-controls='tab-aboutus' to="/AboutUs">About us</LinkRouter>
                         </li>
                         {/* <li className="nav-item dropdown">
                         <a className="nav-link mx-2 dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
