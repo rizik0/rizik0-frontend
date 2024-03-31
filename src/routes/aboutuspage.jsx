7874db175c1265cd49ed45bbc6f401a73656a3a2
Cristian Di Iorio <112891194+cristiandiiorio@users.noreply.github.com>
2024-03-31 21:32:37 +0200

    added aboutsuspage and techpage
    TECHPAGE DA FINIRE
    ABOUTUS PAGE DA PERFEZIONARE

---
+++ src/routes/aboutuspage.jsx
@@ -0,0 +1,71 @@
+import Navbar from '../components/Navbar/Navbar.jsx'
+import '../scss/stlyes.scss'
+
+export default function TechPage(){
+    return(
+        <>
+            <div className="container">
+                <div className="row">
+                    <Navbar id="Navbar"/>
+                </div>
+                <div className="row" style={{paddingTop:'15em'}}>
+                    <section className="bg-light py-3 py-md-5 py-xl-8">
+                        <div className="container">
+                            <div className="row justify-content-center">
+                                <div className="col-12 col-md-10 col-lg-8 col-xl-7 col-xxl-6">
+                                    <h2 className="mb-4 display-5 text-center">IL TEAM</h2>
+                                    <p className="text-secondary mb-5 text-center lead fs-4">Progetto di LTW</p>
+                                    <hr className="w-50 mx-auto mb-5 mb-xl-9 border-dark-subtle"></hr>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="container overflow-hidden">
+                            <div className="row gy-4 gy-lg-0 gx-xxl-5">
+                                <div className="col-4 col-md-6 col-lg-4">
+                                    <div className="card border-0 border-bottom border-primary shadow-sm overflow-hidden">
+                                        <div className="card-body p-0">
+                                            <figure className="m-0 p-0">
+                                                <img className="img-fluid" loading="lazy" src="../../assets/react.svg" alt=""/>
+                                                <figcaption className="m-0 p-4">
+                                                    <h4 className="mb-1">Cristian Di Iorio</h4>
+                                                    <p className="text-secondary mb-0">Studente terzo anno ingegneria informatica</p>
+                                                </figcaption>
+                                            </figure>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-4 col-md-6 col-lg-4">
+                                    <div className="card border-0 border-bottom border-primary shadow-sm overflow-hidden">
+                                        <div className="card-body p-0">
+                                            <figure className="m-0 p-0">
+                                                <img className="img-fluid" loading="lazy" src='../../assets/federico_gerardi.png' alt=""/>
+                                                <figcaption className="m-0 p-4">
+                                                    <h4 className="mb-1">Federico Gerardi</h4>
+                                                    <p className="text-secondary mb-0">Studente terzo anno ingegneria informatica</p>
+                                                </figcaption>
+                                            </figure>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-4 col-md-6 col-lg-4">
+                                    <div className="card border-0 border-bottom border-primary shadow-sm overflow-hidden">
+                                        <div className="card-body p-0">
+                                            <figure className="m-0 p-0">
+                                                <img className="img-fluid" loading="lazy" src="./assets/img/team-img-2.jpg" alt=""/>
+                                                <figcaption className="m-0 p-4">
+                                                    <h4 className="mb-1">Pietro Costanzi Fantini</h4>
+                                                    <p className="text-secondary mb-0">Studente terzo anno ingegneria informatica</p>
+                                                </figcaption>
+                                            </figure>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </>
+    )
+}
--- src/routes/techpage.jsx
+++ src/routes/techpage.jsx
@@ -1,12 +1,48 @@
 import Navbar from '../components/Navbar/Navbar.jsx'
 import '../scss/stlyes.scss'
-import Carousel from '../components/Carousel/Carousel.jsx'
 
 export default function TechPage(){
     return(
         <>
-            <Navbar id="Navbar"/>
-
+            <div class="container">
+                    <div class="row">
+                        <Navbar id="Navbar"/>
+                    </div>
+                    <div class="row" style={{textAlign:'center',color:'black',paddingTop:'10em'}}>
+                        <h1>TECH STACK</h1>
+                        <small style={{paddingBottom:'2.6em'}}> (da frontend a backend) </small>
+                    </div>
+                    <div class="list-group">
+                        <a class="list-group-item list-group-item-action flex-column align-items-center">
+                            <div class="d-flex justify-content-between">
+                            <h4 class="mb-1">React.js</h4>
+                            <small> icona react</small>
+                            </div>
+                            <p class="mb-1">Motivo per il quale abbiamo usato questo</p>
+                        </a>
+                        <a class="list-group-item list-group-item-action flex-column align-items-center">
+                            <div class="d-flex justify-content-between">
+                            <h4 class="mb-1">Vite</h4>
+                            <small> icona vite</small>
+                            </div>
+                            <p class="mb-1">Motivo per il quale abbiamo usato questo</p>
+                        </a>
+                        <a class="list-group-item list-group-item-action flex-column align-items-center">
+                            <div class="d-flex justify-content-between">
+                            <h4 class="mb-1">Python flask</h4>
+                            <small> icona python flask</small>
+                            </div>
+                            <p class="mb-1">Motivo per il quale abbiamo usato questo</p>
+                        </a>
+                        <a class="list-group-item list-group-item-action flex-column align-items-center">
+                            <div class="d-flex justify-content-between">
+                            <h4 class="mb-1">SQL</h4>
+                            <small> icona SQL</small>
+                            </div>
+                            <p class="mb-1">Motivo per il quale abbiamo usato questo</p>
+                        </a>
+                        </div>
+                    </div>
         </>
     )
 }
