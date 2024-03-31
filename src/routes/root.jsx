d7423b664667f800e4bb4912119fb8256f400c45
Pietro Costanzi Fantini <pietrocostanzi39@gmail.com>
2024-03-31 20:38:11 +0200

    Sezione Leaderboard completata

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,11 +1,13 @@
 import Navbar from '../components/Navbar/Navbar.jsx'
 import '../scss/stlyes.scss'
 import Carousel from '../components/Carousel/Carousel.jsx'
+import Leaderboard from '../components/Leaderboard/Leaderboard.jsx'
 
 export default function Root(){
     return(
         <>
             <Navbar id="Navbar"/>
+            <Leaderboard id="Leaderboard" playerName={"Giero"}/>
 
         </>
     )
