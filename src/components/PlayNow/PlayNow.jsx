acfc7fe9d38ddbfc791cdc13cf17377eb1e699cf
Pietro Costanzi Fantini <pietrocostanzi39@gmail.com>
2024-04-01 00:02:47 +0200

    modal-mode fix

--- src/components/PlayNow/PlayNow.jsx
+++ src/components/PlayNow/PlayNow.jsx
@@ -1,15 +1,21 @@
-import { useState } from 'react'
-import { redirect, useNavigate } from 'react-router-dom'
+import { useRef, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import logo from '../../assets/logo_circle.png'
 import './PlayNow.scss'
+import { Modal } from 'bootstrap';
 
 export default function PlayNow(){
+
+    const newModalRef = useRef();
+    const joinModalRef = useRef();
 
     const [name, setName] = useState('')
     const [game_id, setGameId] = useState('')
     const navigate = useNavigate()
 
     const createGame = () => {
+        const modalInstance = Modal.getInstance(newModalRef.current)
+        modalInstance.hide()
         const abortController = new AbortController()
         console.log(name)
         const fetchData = async() => await fetch('http://localhost:3000/api/game/create', {
@@ -24,7 +30,6 @@ export default function PlayNow(){
         fetchData()   
         
         navigate('/lobby')
-        
         return () => {
             abortController.abort()
         }
@@ -32,6 +37,8 @@ export default function PlayNow(){
 
     
     const joinGame = () => {
+        const modalInstance = Modal.getInstance(joinModalRef.current);
+        modalInstance.hide();
         const abortController = new AbortController()
         console.log(name)
         const fetchData = async() => await fetch('http://localhost:3000/api/game/join', {
@@ -62,7 +69,11 @@ export default function PlayNow(){
                 <div className=' row justify-content-md-center'>
                     <div className='col-11'>
                         <div className='playDescription'>
-                            Risk is a classic strategic board game that challenges players to conquer the world through military conquest and tactical prowess. Risk is not merely a game of brute force; it requires careful planning and negotiation. Players must decide whether to expand aggressively, fortify their defenses, or form alliances with other players to achieve their objectives! Are you up to the challenge?
+                            Risk is a classic strategic board game that challenges players to conquer the world through 
+                            military conquest and tactical prowess. Risk is not merely a game of brute force; it requires 
+                            careful planning and negotiation. Players must decide whether to expand aggressively, fortify 
+                            their defenses, or form alliances with other players to achieve their objectives! Are you up to 
+                            the challenge?
                         </div>
                     </div>
                 </div>
@@ -84,8 +95,8 @@ export default function PlayNow(){
                 </div>
             </div>
 
-            <div class="modal fade" id="newModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog">
+            <div class="modal fade" id="newModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" ref={newModalRef}>
+                        <div class="modal-dialog modal-dialog-centered">
                             <div class="modal-content">
                                 <div class="modal-header">
                                     <h1 class="modal-title fs-5" id="exampleModalLabel">Create a game!</h1>
@@ -104,8 +115,8 @@ export default function PlayNow(){
                     </div>
 
 
-            <div class="modal fade" id="joinModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
+            <div class="modal fade" id="joinModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" ref={joinModalRef}>
+                <div class="modal-dialog modal-dialog-centered">
                     <div class="modal-content">
                         <div class="modal-header">
                             <h1 class="modal-title fs-5" id="exampleModalLabel">Join a game!</h1>
