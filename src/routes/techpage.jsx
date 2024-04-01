04678dd6e45c5807e7b233d14671a7e63bdd0420
Pietro Costanzi Fantini <pietrocostanzi39@gmail.com>
2024-04-01 13:26:23 +0200

    sezione regole completeta

--- src/routes/techpage.jsx
+++ src/routes/techpage.jsx
@@ -1,6 +1,5 @@
 import Navbar from '../components/Navbar/Navbar.jsx'
 import '../scss/stlyes.scss'
-import Carousel from '../components/Carousel/Carousel.jsx'
 
 export default function TechPage(){
     return(
