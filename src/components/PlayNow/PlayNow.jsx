4aeaacafe5d9eccce72f85ed8ec5622d21942a57
Pietro Costanzi Fantini <55884404+Nabster101@users.noreply.github.com>
2024-04-01 16:00:16 +0200

    Update PlayNow.jsx

--- src/components/PlayNow/PlayNow.jsx
+++ src/components/PlayNow/PlayNow.jsx
@@ -142,62 +142,6 @@ export default function PlayNow(){
     )
 }
 
-/*
-
-
-<div style={{marginTop: '30rem'}}>
-
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-            Create a game!
-            </button>
-
-            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h1 class="modal-title fs-5" id="exampleModalLabel">Create a game!</h1>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                            <label for="recipient-name" class="col-form-label">Your Name</label>
-                            <input type="text" class="form-control" id="recipient-name" onChange={(s) => setName(s.target.value)} />
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary" onClick={createGame}>Play</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#joinModal">
-            Join a Game
-            </button>
-
-            <div class="modal fade" id="joinModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h1 class="modal-title fs-5" id="exampleModalLabel">Join a game!</h1>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                            <label for="recipient-name" class="col-form-label">Your Name</label>
-                            <input type="text" class="form-control" id="recipient-name" onChange={(s) => setName(s.target.value)} />
-                            <label for="recipient-name" class="col-form-label">Match Code</label>
-                            <input type="text" class="form-control" id="recipient-name" onChange={(s) => setGameId(s.target.value)} />
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary" onClick={joinGame}>Play</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            
-        </div>
-
-*/
 
 
 
